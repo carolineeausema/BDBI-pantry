0499dd5155bd76e2b53666f03bcb3e7a38404f7e
CarolineAusema <52717803+carolineeausema@users.noreply.github.com>
2020-11-21 15:33:26 -0500

    Reads text file into dictionary

--- ReadInventory.py
+++ ReadInventory.py
@@ -1,1 +1,9 @@
 # ReadInventory.py
+amountFile = open("InventoryFile.txt", "r")
+allLines = amountFile.read().split("\n")
+inventory = {}
+for i in allLines:
+	eachLine = i.split(";")
+	if (len(eachLine) == 2):
+		inventory[eachLine[0]] = eachLine[1]
+print(inventory)
