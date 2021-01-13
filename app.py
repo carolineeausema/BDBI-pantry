8f3a2d42b538cda7a0bbca688d3da24255dfc50b
CarolineAusema <52717803+carolineeausema@users.noreply.github.com>
2021-01-13 15:45:35 -0600

    works w database

--- app.py
+++ app.py
@@ -23,20 +23,51 @@ class Item(db.Model):
     content = db.Column(db.String(4096))
     inventory = db.Column(db.Integer)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("BDBI-pantry-web-interface.html", items=Item.query.all())
+    if request.method == "GET":
+        return render_template("BDBI-pantry-web-interface.html", items=Item.query.all())
 
-@app.route('/donation')
+    for i in Item.query.all(): # in all of the food data
+        if (i.content != "test food"): # other than "test food" because there's a space in its name
+            if (request.form[i.content] != i.content): # we want the textform that was changed to the number to be withdrawn
+                i.inventory = i.inventory - int(request.form[i.content])
+                db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/donation', methods=["GET", "POST"])
 def donate():
-    return render_template("BDBI-pantry-web-interface-donation.html")
+    if request.method == "GET":
+        return render_template("BDBI-pantry-web-interface-donation.html", items=Item.query.all())
+
+#    if (request.form["food_label"] != "" and request.form["food_num"] != ""):
+#        item = Item(content=request.form["food_label"], inventory=request.form["food_num"])
+#        db.session.add(item)
+#        db.session.commit()
+
+    for i in Item.query.all(): # in all of the food data
+        if (i.content != "test food"): # other than "test food" because there's a space in its name
+            if (request.form[i.content] != i.content): # we want the textform that was changed to the number to be withdrawn
+                i.inventory = i.inventory + int(request.form[i.content])
+                db.session.commit()
+    return redirect(url_for('donate'))
+
 
 @app.route("/test", methods=["GET", "POST"])
 def test():
     if request.method == "GET":
         return render_template("base.html", items=Item.query.all())
 
-    item = Item(content=request.form["food_label"], inventory=request.form["food_num"])
-    db.session.add(item)
-    db.session.commit()
+    #if (request.form["food_label"] != "" and request.form["food_num"] != ""):
+    #    item = Item(content=request.form["food_label"], inventory=request.form["food_num"])
+    #    db.session.add(item)
+    #    db.session.commit()
+    #    return redirect(url_for('test'))
+
+    for i in Item.query.all(): # in all of the food data
+        if (i.content != "test food"): # other than "test food" because there's a space in its name
+            if (request.form[i.content] != i.content): # we want the textform that was changed to the number to be withdrawn
+                i.inventory = i.inventory - int(request.form[i.content])
+                db.session.commit()
     return redirect(url_for('test'))
